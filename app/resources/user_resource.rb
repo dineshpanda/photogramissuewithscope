b3bb060c5bfd0f8c2cc0f96eedae5d054f98924a
Dinesh Panda <dineshpanda92@gmail.com>
2020-08-04 19:16:12 +0530

    Generate graphiti resources for users

---
+++ app/resources/user_resource.rb
@@ -0,0 +1,18 @@
+class UserResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :email, :string
+  attribute :password, :string
+  attribute :username, :string
+  attribute :avatar, :string
+  attribute :first_name, :string
+  attribute :last_name, :string
+  attribute :bio, :string
+  attribute :age, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
