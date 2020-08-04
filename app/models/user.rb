60dd223b276e5fc712c2686116a3a3cf80463020
Dinesh Panda <dineshpanda92@gmail.com>
2020-08-04 19:14:23 +0530

    Validate uniqueness of users.username

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   # Validations
 
+  validates :username, :uniqueness => { :scope => [:first_name, :last_name, :age], :allow_blank => true, :case_sensitive => true, :message => "username should be unique always" }
+
   validates :username, :presence => { :message => "username can not be blank!" }
 
   validates :username, :length => { :minimum => 5, :maximum => 20, :allow_blank => true }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,8 @@ RSpec.describe User, type: :model do
 
     describe "Validations" do
 
+    it { should validate_uniqueness_of(:username).scoped_to(:first_name).scoped_to(:last_name).scoped_to(:age).allow_nil.case_insensitive.with_message('username should be unique always') }
+
     it { should validate_presence_of(:username).with_message('username can not be blank!') }
 
     it { should validate_length_of(:username).is_at_least(5).is_at_most(20).allow_nil }
