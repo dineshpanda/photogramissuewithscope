71b071ec5082ca163e1c930668b9143d24807e85
Dinesh Panda <dineshpanda92@gmail.com>
2020-08-04 19:14:38 +0530

    Direct association between users and friend_requests

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,6 +1,10 @@
 class FriendRequest < ApplicationRecord
   # Direct associations
 
+  belongs_to :follower,
+             :class_name => "User",
+             :foreign_key => "sender_id"
+
   belongs_to :recipient,
              :class_name => "User",
              :counter_cache => :received_friend_requests_count
