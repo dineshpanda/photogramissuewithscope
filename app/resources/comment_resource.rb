e06155d1728c88987adca44903c56bc41304128e
Dinesh Panda <dineshpanda92@gmail.com>
2020-08-04 19:16:22 +0530

    Generate graphiti resources for comments

---
+++ app/resources/comment_resource.rb
@@ -0,0 +1,16 @@
+class CommentResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :body, :string
+  attribute :commenter_id, :integer
+  attribute :photo_id, :integer
+  attribute :schedule_time, :string
+  attribute :pin, :boolean
+  attribute :schedule_date, :date
+
+  # Direct associations
+
+  # Indirect associations
+
+end
