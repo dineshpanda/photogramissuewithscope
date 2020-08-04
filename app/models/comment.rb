419e4ed7c6011a23d91e1b5b000bb9b75a1c6754
Dinesh Panda <dineshpanda92@gmail.com>
2020-08-04 19:14:47 +0530

    Add scope late_comments to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -15,6 +15,8 @@ class Comment < ApplicationRecord
 
   # Scopes
 
+  scope :late_comments, -> { where("comments.schedule_time > :query", query: "2020-08-04 21:30:00 +0530" ).where("comments.schedule_date >= :query", query: "2020-08-01T00:00:00+00:00" ) }
+
   scope :imp_comments, -> { where(:pin => [true]) }
 
 end
